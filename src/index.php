72654ce62892e948792a115832f026f3bad78248
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-10-12 13:15:56 +0300

    Add .env files & add basic API url generation

--- src/index.php
+++ src/index.php
@@ -1,36 +1,31 @@
-<?php
+<html>
+    <head>
+        <title>Stock App</title>
+    </head>
 
-echo "Hello, World!<br /><br />";
-echo "You're using PHP " . phpversion() . ".<br />";
+    <body>
 
-// Test database
-$serverName = "db";
-$databaseName = "root_db";
-$dbUsername = "root";
-$dbPassword = "root";
-$pdoOpts = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
-$serverUri = "mysql:host={$serverName};dbname={$databaseName};charset=utf8mb4";
+    <?php
+    // Make a request to Alpha Vantage ========================================
+    // Make sure the API key is present as an environment variable
+    if (!array_key_exists("ALPHA_VANTAGE_API_KEY", $_ENV)) {
+        die("API Key not found!");
+    }
+    $base_url = "https://www.alphavantage.co/query?";
 
-echo "Connecting to: <a href=\"\">{$serverUri}</a><br />";
+    // Build parameter string
+    $parameters = [
+        "function" => "TIME_SERIES_DAILY",
+        "symbol" => "AAPL",
+        "outputsize" => "compact",
+        "apikey" => $_ENV["ALPHA_VANTAGE_API_KEY"],
+    ];
 
-try {
-    $pdo = new PDO($serverUri, $dbUsername, $dbPassword, $pdoOpts);
-} catch (Exception $ex) {
-    echo "Could not connect to the database: " . $ex->getMessage() . "<br />";
-    error_log($ex->getMessage());
-    die();
-}
+    // Build final GET Request url
+    $url = $base_url . http_build_query($parameters);
 
-echo "Database OK<br />";
+    echo "<a href=$url>Click here</a>";
+    ?>
 
-try {
-    $dbVersion = $pdo->query("SELECT VERSION()")->fetch()["VERSION()"];
-} catch (Exception $ex) {
-    echo "Could not connect to the database version info: " .
-        $ex->getMessage() .
-        "<br />";
-    error_log($ex->getMessage());
-    die();
-}
-
-echo "Detected database: {$dbVersion}<br />";
+    </body>
+</html>
