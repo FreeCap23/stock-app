657ee48d4172a09ad3c9de942077873fb7f5ed49
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-10-13 15:08:40 +0300

    Add form to handle get request url

--- src/index.php
+++ src/index.php
@@ -5,26 +5,33 @@
 
     <body>
 
+    <form action="" method="GET">
+    Symbol: <input type="text" name="symbol"><br>
+    <input type="submit" name="submitBtn">
+    </form>
+
     <?php
-    // Make a request to Alpha Vantage ========================================
-    // Make sure the API key is present as an environment variable
-    if (!array_key_exists("ALPHA_VANTAGE_API_KEY", $_ENV)) {
-        die("API Key not found!");
-    }
-    $base_url = "https://www.alphavantage.co/query?";
+      // Check if form was submitted
+      if (isset($_GET["submitBtn"])) {
+        // Make a request to Alpha Vantage ========================================
+        // Make sure the API key is present as an environment variable
+        if (!array_key_exists("ALPHA_VANTAGE_API_KEY", $_ENV)) {
+            die("API Key not found!");
+        }
+        $base_url = "https://www.alphavantage.co/query?";
 
-    // Build parameter string
-    $parameters = [
-        "function" => "TIME_SERIES_DAILY",
-        "symbol" => "AAPL",
-        "outputsize" => "compact",
-        "apikey" => $_ENV["ALPHA_VANTAGE_API_KEY"],
-    ];
+        // Build parameter string
+        $parameters = [
+            "function" => "TIME_SERIES_DAILY",
+            "symbol" => $_GET["symbol"],
+            "outputsize" => "compact",
+            "apikey" => $_ENV["ALPHA_VANTAGE_API_KEY"],
+        ];
 
-    // Build final GET Request url
-    $url = $base_url . http_build_query($parameters);
-
-    echo "<a href=$url>Click here</a>";
+        // Build final GET Request url
+        $url = $base_url . http_build_query($parameters);
+        echo "<a href=$url>Click here</a>";
+      }
     ?>
 
     </body>
