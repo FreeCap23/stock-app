42883cdc05cbf2b1fb9f46d96edd77786878ab1d
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-10-25 19:56:03 +0300

    Modify primary key

--- init/init.sql
+++ init/init.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS daily_ohlcv (
-    symbol  CHAR(6) PRIMARY KEY NOT NULL,
+    symbol  CHAR(6) NOT NULL,
     date    DATE NOT NULL,
     open    DECIMAL(10, 3) NOT NULL,
     high    DECIMAL(10, 3) NOT NULL,
     low     DECIMAL(10, 3) NOT NULL,
     close   DECIMAL(10, 3) NOT NULL,
-    volume  INT UNSIGNED NOT NULL
+    volume  INT UNSIGNED NOT NULL,
+    PRIMARY KEY (symbol, date)
 );
