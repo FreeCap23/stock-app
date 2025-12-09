752083190537e8d25eb1e2fd2f263cdbe386d956
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-12-09 10:35:52 +0200

    Add ticker_metadata table

--- init/init.sql
+++ init/init.sql
@@ -6,5 +6,16 @@ CREATE TABLE IF NOT EXISTS daily_ohlcv (
     low     DECIMAL(10, 3) NOT NULL,
     close   DECIMAL(10, 3) NOT NULL,
     volume  INT UNSIGNED NOT NULL,
-    PRIMARY KEY (symbol, date)
+    PRIMARY KEY (symbol, date),
+    FOREIGN KEY(symbol) REFERENCES ticker_metadata(symbol)
 );
+
+CREATE TABLE IF NOT EXISTS ticker_metadata (
+    symbol          CHAR(6) NOT NULL,
+    name            VARCHAR(64) NOT NULL,
+    description     TEXT,
+    start_date      DATE NOT NULL,
+    end_date        DATE,
+    exchange_code   VARCHAR(32) NOT NULL,
+    PRIMARY KEY     (symbol)
+)
